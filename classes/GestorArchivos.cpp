6b62e3bd88b1ee115049f2fcca842d85b2eaa833
ivancosse-ice <ivan.cosse@alumnos.ucn.cl>
2026-05-04 14:26:15 -0400

    GestorArchivos.cpp
    Aplicacion de los metodos relatados anteriormente en el .h (cargar y guardar Estado)

--- classes/GestorArchivos.cpp
+++ classes/GestorArchivos.cpp
@@ -52,3 +52,57 @@ ListaEnlazada<Cancion> GestorArchivos::cargarCanciones()
 
     return canciones;
 }
+
+void GestorArchivos::guardarEstado(Reproductor& rep)
+{
+    ofstream file("data/status.cfg");
+
+    if (!file.is_open())
+    {
+        cout << "Error al guardar estado" << endl;
+        return;
+    }
+
+    file << "current_song=" << rep.getIndexActual() << endl;
+    file << "is_playing=" << rep.getPlaying() << endl;
+    file << "shuffle=" << rep.getMix() << endl;
+    file << "repeat_mode=" << rep.getRepeatMode() << endl;
+
+    file.close();
+}
+
+void GestorArchivos::cargarEstado(Reproductor& rep)
+{
+    ifstream file("data/status.cfg");
+
+    if (!file.is_open())
+    {
+        rep.SetEstado(0, false, false, 0);
+        return;
+    }
+
+    string line;
+    int current = 0;
+    int playing = 0;
+    int mixRand = 0;
+    int repeat = 0;
+
+    while (getline(file, line))
+    {
+        if (line.find("current_song=") == 0)
+            current = stoi(line.substr(13));
+
+        else if (line.find("is_playing=") == 0)
+            playing = stoi(line.substr(11));
+
+        else if (line.find("shuffle=") == 0)
+            mixRand = stoi(line.substr(8));
+
+        else if (line.find("repeat_mode=") == 0)
+            repeat = stoi(line.substr(12));
+    }
+
+    file.close();
+
+    rep.SetEstado(current, playing, mixRand, repeat);
+}
