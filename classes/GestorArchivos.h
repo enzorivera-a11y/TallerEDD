6c567cfd92b54edc06d00462af1d2d4c692f5e22
ivancosse-ice <ivan.cosse@alumnos.ucn.cl>
2026-05-04 14:22:35 -0400

    GestorArchivos.h
    Puse los voids cargar y guardar archivo para modificar el status cuando lo requiera

--- classes/GestorArchivos.h
+++ classes/GestorArchivos.h
@@ -5,12 +5,21 @@
 
 #include "Cancion.h"
 #include "../data_Structures/ListaEnlazada.h"
+#include "Reproductor.h"
 
 class GestorArchivos
 {
     public:
     ListaEnlazada<Cancion> cargarCanciones();
+    void guardarEstado(Reproductor& rep);
+    void cargarEstado(Reproductor& rep);
+
 };
+
+
+
+#endif //TALLEREDD_GESTORARCHIVOS_H
+
 
 
 #endif //TALLEREDD_GESTORARCHIVOS_H
